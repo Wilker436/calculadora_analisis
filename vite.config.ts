8060d687c2c735e28294d0bc809cfc7fb0a33efa
wilker436 <wikerpachecoperez@gmail.com>
2025-10-27 21:07:25 -0500

    sin base

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,4 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
-  base: '/calculadora_analisis/'
 })
